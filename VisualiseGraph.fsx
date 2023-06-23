1d4eb201c7b46098a9ae8d740e22f2a71e0ad00d
Harry McCarney <hmccarney@gmail.com>
2023-06-23 06:45:19 +0100

    updated readme

--- VisualiseGraph.fsx
+++ VisualiseGraph.fsx
@@ -1,26 +1,27 @@
 #r "nuget: Cyjs.NET"
-#load "./BuildSlackGraph.fsx"
+//#load "./BuildSlackGraph.fsx"
 
 #load "./BuildMeetsGraph.fsx"
 
 
 open System
 open Cyjs.NET
+//open BuildSlackGraph
 open BuildMeetsGraph
 
 
-//let graph = buildGraph ["tradekart"] (DateTime.Now.AddMonths(-6)) (DateTime.Now)
-let graph = buildGraph()
+//let graph = buildGraph [] (DateTime.Now.AddMonths(-6)) (DateTime.Now) //slacck
+let graph = buildGraph() //Meeets
 
 let nodeElements = 
     let format name degree = [ CyParam.color "blue"; CyParam.label (string name); CyParam.weight degree ]
     graph.GetVertices()
-    |> Array.map(fun n -> Elements.node n (format n (graph.Degree n)))
+    |> Array.mapi(fun i n -> Elements.node n (format i (graph.Degree n)))
 
 
 let edgeElements = 
     graph.GetEdges()
-    |> Array.filter(fun (v1, v2, weight) -> weight > 30000.0)
+    |> Array.filter(fun (v1, v2, weight) -> weight > 100000.0) // set edge weight filter
     |> Array.map (fun (v1, v2, weight) ->
         Elements.edge (string v1 + "_" + string v2) (string v1) (string v2) [ CyParam.weight (weight) ])
 
@@ -42,6 +43,6 @@ CyGraph.initEmpty ()
          
         ]
     |> CyGraph.withSize (1200, 800)
-    //|> CyGraph.withLayout (Layout.initCose (Layout.LayoutOptions.Cose(NodeOverlap = 400)))
-    |> CyGraph.withLayout (Layout.initCircle (Layout.LayoutOptions.Generic()))
+    |> CyGraph.withLayout (Layout.initCose (Layout.LayoutOptions.Cose(NodeOverlap = 400)))
+    //|> CyGraph.withLayout (Layout.initCircle (Layout.LayoutOptions.Generic()))
 |> CyGraph.show
